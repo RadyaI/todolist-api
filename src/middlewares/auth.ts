c1b3262fe73c09231754e643d629532e21212867
RadyaI <radyaiftikhar@gmail.com>
2025-02-03 21:37:10 +0700

    feat: check admin function

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,11 +3,11 @@ import { errorRes } from './response'
 import * as jwt from "jsonwebtoken"
 import { JWT_TOKEN } from '../secret'
 
-export function jwtAuth(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers.authorization?.replace("Bearer ", "")
+export function jwtAuth(req: Request, res: Response, next: NextFunction): any {
+    const token = req.headers.authorization?.replace("Bearer ", "")!
 
     if (!token) {
-        return res.status(401).json(errorRes("error", 401, "INVALID TOKEN", "Token not found or invalid"))
+        res.status(401).json(errorRes("error", 401, "INVALID TOKEN", "Token not found or invalid"))
     }
 
     jwt.verify(token, JWT_TOKEN, (err, decode) => {
@@ -20,4 +20,14 @@ export function jwtAuth(req: Request, res: Response, next: NextFunction) {
         next()
     })
 
+}
+
+export function admin(req: Request, res: Response, next: NextFunction): any {
+    const userData = (req as any).auth
+
+    if (userData.role !== "ADMIN") {
+        return res.status(401).json(errorRes("error", 401, "UNAUTHORIZED_ACCESS", `Trying to access ADMIN with ${userData.role} role`))
+    }
+
+    next()
 }
