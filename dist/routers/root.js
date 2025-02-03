ef85d4f988a198fbcba7decd6734d7beb60630d3
RadyaI <radyaiftikhar@gmail.com>
2025-02-03 13:48:19 +0700

    feat: ip endpoint

--- dist/routers/root.js
+++ dist/routers/root.js
@@ -7,6 +7,9 @@ const express_1 = __importDefault(require("express"));
 const authRouter_1 = __importDefault(require("./authRouter"));
 const rootRouter = (0, express_1.default)();
 rootRouter.use("/auth", authRouter_1.default);
+rootRouter.get("/ip", (req, res) => {
+    res.json(req.ip);
+});
 rootRouter.use("*", (req, res) => {
     res.status(404).json({ Route: "Not Found" });
 });
--- src/routers/root.ts
+++ src/routers/root.ts
@@ -5,6 +5,11 @@ const rootRouter: Router = router()
 
 rootRouter.use("/auth", authRouter)
 
+rootRouter.get('/ip', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    res.json(ip);
+})
+
 rootRouter.use("*", (req, res) => {
     res.status(404).json({ Route: "Not Found" })
 })
